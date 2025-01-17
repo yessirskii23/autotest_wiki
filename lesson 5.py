7e40ac195a8e682efe7b018799e35ad098e25026
yessirskii23 <adam.kadmoon78@gmail.com>
2025-01-17 12:37:57 +0300

    Обновление структуры проекта и удаление старой директории

--- lesson 5.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-# Указываем путь к драйверу (например, для Chrome)
-driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
-
-driver.get('https://www.google.com')
-print(driver.title)
-
-driver.quit()
--- lesson_2/1_firefoxdriver.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.service import Service
-
-service = Service(executable_path=GeckoDriverManager().install())
-driver = webdriver.Firefox(service=service)
